8e3c82894886acca70ea50c12a0001c807a0af8c
Gabe N <nelsong1997@gmail.com>
2020-03-22 13:53:00 -0400

    the zombies take over and then crash the app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ class App extends React.Component {
 	constructor() {
         super();
         this.state = {
+            roundsCompleted: 0,
             humanOccupiedHexes: [],
-            zombieOccupiedHexes: []
+            zombieOccupiedHexes: [],
+            numZombieArms: 3,
+            humanCount: 50,
+            zombieCount: 1
         }
-        this.start = this.start.bind(this);
+        this.nextRound = this.nextRound.bind(this);
+        this.restart = this.restart.bind(this);
     }
 
     displayBoard() {
@@ -39,7 +44,7 @@ class App extends React.Component {
                 if (this.state.zombieOccupiedHexes.includes(10*j + i)) {
                     theZombies.push(
                         <text
-                            x={1.5*j + .55} y={k+1.3} opacity="0.5"
+                            x={1.5*j + .5} y={k+1.32} opacity="0.5"
                             fontFamily="Arial" fontSize="1.75px" fill="red" key={10*j+i}>o
                         </text>
                     )
@@ -55,20 +60,66 @@ class App extends React.Component {
         )
     }
 
-    start() {
-        let humanStartCount = 50
-        let zombieStartCount = 1
+    displayControls(stateObject) {
+        let theButtons = []
+        if (stateObject.roundsCompleted===0) {
+            theButtons = <button onClick={this.nextRound}>start</button>
+        } else {
+            theButtons = [
+                <div className="col-item" key="0">
+                    <button onClick={this.nextRound}>next round</button>
+                </div>,
+                <div className="col-item" key="1">
+                    <button onClick={this.restart}>restart</button>
+                </div>
+            ]
+        }
+
+        return (
+            <div id="controls">
+                <div id="col-0" className="column"> {/* buttons*/}
+                    {theButtons}
+                </div>
+                <div id="col-1" className="column">   {/* initial condition settings*/}
+                    <div className="col-item">
+                        <label>Human Start Count</label>
+                        <input type="number" className="num-input" defaultValue="50"/>
+                    </div>
+                    <div className="col-item">
+                        <label>Zombie Start Count</label>
+                        <input type="number" className="num-input" defaultValue="1"/>
+                    </div>
+                </div>
+                <div id="col-2" className="column">   {/* status*/}
+                    <div className="col-item">
+                        <label>Human Count: {stateObject.humanCount}</label>
+                    </div>
+                    <div className="col-item">
+                        <label>Zombie Count: {stateObject.zombieCount}</label>
+                    </div>
+                    <div className="col-item">
+                        <label>Round #: {stateObject.roundsCompleted}</label>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    nextRound() {
+        let stateObject = this.state
+        let humanCount = stateObject.humanCount
+        let zombieCount = stateObject.zombieCount
         let humanUnoccupiedHexes = makeCountArray(100)
         let zombieUnoccupiedHexes = makeCountArray(100)
         let humanOccupiedHexes = []
         let zombieOccupiedHexes = []
-        for (let i=0; i<humanStartCount; i++) {
+        for (let i=0; i<humanCount; i++) {
             let chosenIndex = randomInteger(0, 99-i)
             let chosenHex = humanUnoccupiedHexes[chosenIndex]
             humanOccupiedHexes.push(chosenHex)
             humanUnoccupiedHexes = deleteAtIndex(humanUnoccupiedHexes, chosenIndex)
         }
-        for (let i=0; i<zombieStartCount; i++) {
+        for (let i=0; i<zombieCount; i++) {
             let chosenIndex = randomInteger(0, 99-3*i)
             let chosenHex = zombieUnoccupiedHexes[chosenIndex]
 
@@ -121,12 +172,36 @@ class App extends React.Component {
             zombieUnoccupiedHexes = deleteAtIndex(zombieUnoccupiedHexes, chosenIndex)
             zombieUnoccupiedHexes = deleteAtIndex(zombieUnoccupiedHexes, arm0Index)
             zombieUnoccupiedHexes = deleteAtIndex(zombieUnoccupiedHexes, arm1Index)
-            console.log(chosenHex)
         }
+
+        let newInfections = 0
+        for (let hex of zombieOccupiedHexes) {
+            if (humanOccupiedHexes.includes(hex)) newInfections++
+            console.log(newInfections)
+        }
+        humanCount = humanCount - newInfections
+        zombieCount = zombieCount + newInfections
+
         this.setState(
             {
                 humanOccupiedHexes: humanOccupiedHexes.sort((a, b) => a-b),
-                zombieOccupiedHexes: zombieOccupiedHexes.sort((a, b) => a-b)
+                zombieOccupiedHexes: zombieOccupiedHexes.sort((a, b) => a-b),
+                roundsCompleted: stateObject.roundsCompleted + 1,
+                zombieCount: zombieCount,
+                humanCount: humanCount
+            }
+        )
+    }
+
+    restart() {
+        this.setState(
+            {
+                roundsCompleted: 0,
+                humanOccupiedHexes: [],
+                zombieOccupiedHexes: [],
+                numZombieArms: 3,
+                humanCount: 50,
+                zombieCount: 1
             }
         )
     }
@@ -135,26 +210,35 @@ class App extends React.Component {
         return (
             <div id="main">
                 {this.displayBoard()}
-                <br/>
-                <button onClick={this.start}>start</button>
+                {this.displayControls(this.state)}
             </div>
         )
     }
-} 
+}
+
+//------------------------------------------------------------------------------------------------------------------------//
+
+//------------------------------------------------------------------------------------------------------------------------//
 
 function randomInteger(min, max) {
     let range = max - min + 1
     return Math.floor(range*(Math.random())) + min
 }
 
-function deleteAtIndex(array, index) {
+function deleteAtIndex(array, index) { //someArray===["apple", "banana", "cheese"]; deleteAtIndex(someArray, 1) => ["apple", "cheese"]
     return array.slice(0, index).concat(array.slice(index + 1, array.length))
 }
 
-function makeCountArray(length) {
+function makeCountArray(length) { //makeCountArray(4) => [0,1,2,3]
     let returnArray = []
     for (let i=0; i<length; i++) returnArray.push(i)
     return returnArray
 }
 
 export default App;
+
+//history
+//arm count
+//zombie mortality
+//vaxx
+//start counts
